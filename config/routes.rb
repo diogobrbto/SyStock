7158665ce87f229354d269618bb8282020f6019a
Diogo Barbirato <diogobarbirato@gmail.com>
2023-09-07 09:06:59 -0300

    routes_names_links_menu

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,22 @@
 Rails.application.routes.draw do
-  root 'main#home'
+  get 'categories/index'
+  get 'categories/show', to: 'categories#show', as: 'show_categorie'
+  get 'categories/new', to: 'categories#new', as: 'new_categorie'
+  get 'categories/create'
+  get 'categories/edit'
+  get 'categories/update'
+  get 'categories/destroy'
 
-  resources :categories
-  resources :products
+  get 'products/index'
+  get 'products/show', to: 'products#show', as: 'show_product'
+  get 'products/new', to: 'products#new', as: 'new_product'
+  get 'products/create'
+  get 'products/edit'
+  get 'products/update'
+  get 'products/destroy'
+
   devise_for :users
+  root 'main#home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
